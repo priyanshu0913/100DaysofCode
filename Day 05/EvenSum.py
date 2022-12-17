a2571bbbccd266e5bc5a9e98483ffcee27a6d5da
Priyanshu Saraswat <118011902+priyanshu0913@users.noreply.github.com>
2022-12-17 21:46:41 +0530

    Python List

---
+++ Day 05/EvenSum.py
@@ -0,0 +1,6 @@
+# Program to print sum of 100 even numbers
+
+even_sum = 0
+for number in range(2, 101, 2):
+    even_sum += number
+print(even_sum)
---
+++ Day 05/Fruit.py
@@ -0,0 +1,5 @@
+# Program to print contents of list
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
